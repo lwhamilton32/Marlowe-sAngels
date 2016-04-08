92afb83f62279c370bad173764d38c43a2faac68
lwhamilton32 <lwhamilton32@yahoo.com>
2016-04-08 20:10:06 +0100

    Added files via upload

---
+++ cardiff.php
@@ -0,0 +1,17 @@
+
+<html> 
+<head> 
+<title> Cardiff University </title>        
+</head>
+    
+<body>  
+<php
+     echo ="<h1>Cardiff University</h1>";></php>
+    <p> we should be able to put the BUCS table here, well in the centre anyways </p>
+<p> Click on your sport, and login to access your private page. </p>
+<form action="netball.hmtl"> Username:<input type="text" name="usrname"><br><input type="submit" value="submit"> </form>  
+    
+</body>
+</html>
+
+
